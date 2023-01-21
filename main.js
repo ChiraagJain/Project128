31a19123a50cdd1ac5ef18ce32b621076f549977
Chiraag Jain <78903401+ChiraagJain@users.noreply.github.com>
2023-01-21 18:41:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload(){
+    song1 = loadSound("enemy.mp3");
+    song2 = loadSound("toxic.mp3");
+}
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function draw(){
+    image(video,0,0,600,500);
+}
+function modelLoaded(){
+    console.log("pose net is started");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left wrist x = "+leftWristX);
+        console.log("Left wrist y = "+leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right wrist x = "+rightWristX);
+        console.log("Right wrist y = "+rightWristY);
+    }
+}
